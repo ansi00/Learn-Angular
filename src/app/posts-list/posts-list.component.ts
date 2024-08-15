298f427f1b65f5097d4cf181ec75889875869020
Anas Abbas <anasabbasmalik88@gmail.com>
2024-08-15 16:45:45 +0500

    Passing Data from Parent to Child Component and Vice Versa

--- src/app/posts-list/posts-list.component.ts
+++ src/app/posts-list/posts-list.component.ts
@@ -1,13 +1,24 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, EventEmitter, Output } from '@angular/core';
+import { CardComponent } from '../card/card.component';
 
 @Component({
   selector: 'app-posts-list',
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './posts-list.component.html',
   styleUrl: './posts-list.component.css'
 })
 export class PostsListComponent {
- @Input() Title1 : string = '';
- @Input() userStatus : Boolean = true; 
+//  @Input() Title1 : string = '';
+//  @Input() userStatus : Boolean = true; 
+
+ childMessage : string = "Hello from child component";
+
+ parentMessage : string = "Message from child to parent using click emitter";
+
+ @Output() messageEvent = new EventEmitter();
+
+ SendMessage(){
+  this.messageEvent.emit(this.parentMessage);
+ }
 }
