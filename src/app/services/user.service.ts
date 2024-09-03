e8bd62e1789f456b2502acb1c1f6e57e8fbe6866
Anas Abbas <anasabbasmalik88@gmail.com>
2024-09-03 12:47:25 +0500

    Services in Angular

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,23 @@
+
+ export class UserService {
+    users : Array <any> =  [{
+        age : 30 ,
+         name : 'Joe Doe',
+       email : 'JoeDoe@gmail.com'
+       },
+       {
+         age : 20 ,
+         name : 'John Smith',
+       email : 'JoeSmith@gmail.com'
+       },
+       {
+         age : 50 ,
+          name : 'Sam Walker',
+        email : 'SamWalker@gmail.com'
+        },
+        {
+         age : 40 ,
+          name : 'Lisa David',
+        email : 'LisaDavid@gmail.com'
+        },]
+}
